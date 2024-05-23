596719de5d6ed7838466811cc3fd890f3c0a54b3
Sakib Rahman <rahmans@myumanitoba.ca>
2024-05-23 15:39:39 -0400

    Default detector version should be main (#19)
    ### Briefly, what does this PR introduce?
    Default detector version should be main. Otherwise, it affects
    production timing calculation.
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    
    ### Does this PR change default behavior?

--- scripts/run.sh
+++ scripts/run.sh
@@ -37,7 +37,7 @@ test -f .machine.ad && cat .machine.ad
 
 # Load container environment (include ${DETECTOR_VERSION})
 export DETECTOR_CONFIG_REQUESTED=${DETECTOR_CONFIG:-}
-source /opt/detector/epic-${DETECTOR_VERSION:-nightly}/bin/thisepic.sh
+source /opt/detector/epic-${DETECTOR_VERSION:-main}/bin/thisepic.sh
 export DETECTOR_CONFIG=${DETECTOR_CONFIG_REQUESTED:-${DETECTOR_CONFIG:-$DETECTOR}}
 
 # Argument parsing
@@ -108,7 +108,7 @@ INPUT_PREFIX=${INPUT_DIR/\/*/}
 TAG=${INPUT_DIR/${INPUT_PREFIX}\//}
 INPUT_DIR=${BASEDIR}/EVGEN/${TAG}
 mkdir -p ${INPUT_DIR}
-TAG=${DETECTOR_VERSION}/${DETECTOR_CONFIG}/${TAG}
+TAG=${DETECTOR_VERSION:-main}/${DETECTOR_CONFIG}/${TAG}
 
 # Copy input file from xrootd
 xrdcp -f ${XRDRURL}/${XRDRBASE}/${INPUT_FILE} ${INPUT_DIR}
